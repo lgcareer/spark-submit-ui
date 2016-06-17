b3aa676459d084dbc61d7c21ce568dcd16792b9b
liangkai1 <liangkai1@staff.weibo.com>
2016-06-17 18:45:04 +0800

    修改sql语句

--- conf/evolutions/1.sql
+++ conf/evolutions/1.sql
@@ -5,7 +5,8 @@
 create table user (
   email                     varchar(255) not null primary key,
   name                      varchar(255) not null,
-  password                  varchar(255) not null
+  password                  varchar(255) not ,
+  status                    int(4)  NOT NULL DEFAULT 0
 );
 
 # --- !Downs
