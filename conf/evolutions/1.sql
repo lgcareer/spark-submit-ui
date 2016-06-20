70d7ae833a77e1a04f2626994e88443bcded3235
liangkai1 <liangkai1@staff.weibo.com>
2016-06-20 12:02:48 +0800

    新增网址

--- conf/evolutions/1.sql
+++ conf/evolutions/1.sql
@@ -1,13 +1,13 @@
 # --- Initiali DB schema for identity management
 
 # --- !Ups
-
 create table user (
   email                     varchar(255) not null primary key,
   name                      varchar(255) not null,
   password                  varchar(255) not ,
   status                    int(4)  NOT NULL DEFAULT 0
 );
+
 
 # --- !Downs
 
