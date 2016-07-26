9748392bd4e94a9f1ddf70efca42bb1b20df7b42
田瑞林 <ruilin@staff.weibo.com>
2016-07-26 20:39:12 +0800

    修改节点服务table

--- app/controllers/monitor/Serviceplan.scala
+++ app/controllers/monitor/Serviceplan.scala
@@ -20,4 +20,10 @@ object Serviceplan extends Controller with Secured{
     Ok(views.html.widgetsfun())
   }
 
+  def graph = IsAuthenticated {username => implicit request =>
+    //spark graph 处理
+    val ip = "10.75.16.239"
+    Ok(views.html.graph(ip))
+  }
+
 }
