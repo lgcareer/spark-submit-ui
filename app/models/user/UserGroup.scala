6a959f4e36aa2c3a7ebd15fc5df2720753b7bc26
jhazheng <jiazheng1@staff.weibo.com>
2016-08-06 07:32:10 +0800

    change ui bigs.

---
+++ app/models/user/UserGroup.scala
@@ -0,0 +1,56 @@
+package models
+
+import anorm.SqlParser._
+import play.api.db.DB
+import anorm._
+import play.api.Play.current
+/**
+  * Created by liangkai1 on 16/8/5.
+  */
+
+case class  UserGroup(email:String,name:String,group:String,queue:String)
+case class  Group(userGroup: Seq[UserGroup])
+object UserGroup {
+
+  val simple = {
+      get[String]("user_group.email") ~
+      get[String]("user_group.name") ~
+      get[String]("user_group.password")~
+      get[String]("user_group.queue") map {
+      case email ~ name ~ group ~ queue => UserGroup(email,name, group,queue)
+    }
+  }
+
+
+  /**
+    *查找所有用户组
+    * @return
+    */
+  def findGroup : Seq[UserGroup] = {
+    DB.withConnection { implicit connection =>
+      play.api.db.DB.withConnection { implicit connection =>
+        SQL("select * from user_group").as(UserGroup.simple *)
+      }
+    }
+  }
+
+
+  /**
+    * 移除用户组
+    * @param email
+    * @return
+    */
+  def removeGroup(email:String): Int ={
+    play.api.db.DB.withConnection { implicit connection =>
+      SQL(
+        """
+         delete  from user_group where
+         email = {email}
+        """).on(
+        'email -> email
+        ).executeUpdate()
+    }
+  }
+
+
+}
