a5201a2851a89985a6f7bbe4877f9cc632a073a0
田瑞林 <ruilin@staff.weibo.com>
2016-08-25 14:36:10 +0800

    添加个人任务交互/组使用资源占比等

--- app/models/user/UserGroup.scala
+++ app/models/user/UserGroup.scala
@@ -1,10 +1,9 @@
 package models
 
 import anorm.SqlParser._
-import play.api.db.DB
 import anorm._
-import play.api.Logger
 import play.api.Play.current
+import play.api.db.DB
 /**
   * Created by liangkai1 on 16/8/4.
   * 用户组数据操作相关
