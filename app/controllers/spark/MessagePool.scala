a1123066d55c0a8a44c3f0a944dacb6eb5541a90
liangkai1 <kingekinge@163.com>
2016-08-31 17:32:39 +0800

    任务推送完成

---
+++ app/controllers/spark/MessagePool.scala
@@ -0,0 +1,47 @@
+package controllers.spark
+
+import akka.actor.Actor
+import akka.actor.ActorRef
+import akka.actor.Props
+import scala.util.Random
+import akka.actor.PoisonPill
+import akka.actor.Kill
+import akka.actor.AllForOneStrategy
+import akka.actor.SupervisorStrategy._
+import akka.actor.Terminated
+
+
+
+case class ChangeDirection(coordinates: Int)
+case class NewSnake(snakeName: String)
+
+object SnakePool {
+  def props(webSocketChannel: ActorRef): Props = Props(new SnakePool(webSocketChannel))
+}
+
+
+class SnakePool(webSocketChannel: ActorRef) extends Actor {
+  
+  override val supervisorStrategy = AllForOneStrategy() {
+    case anyException => Stop
+  }
+  
+  val apple = context.actorOf(Apple.props(new Random(), webSocketChannel))
+  apple ! NewApple
+  var snakes = Map.empty[String, ActorRef]
+  def receive = {
+    case NewSnake(snakeName) => {
+      val snake = context.actorOf(Snake.props(apple, snakeName, webSocketChannel), snakeName)
+      snake ! Start
+      snakes += (snakeName -> snake)
+      context.watch(snake)
+    }
+    case t: Terminated => self ! PoisonPill
+    case cd: ChangeDirection => snakes.map(snakesByName => snakesByName._2 ! cd)
+  }
+  
+  override def postStop() {
+    webSocketChannel ! Send("Game Over")
+    snakes.map(snakeByName => snakeByName._2 ! Kill)
+  }
+}
