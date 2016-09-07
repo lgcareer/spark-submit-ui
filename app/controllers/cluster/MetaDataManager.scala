466401f0bd6632fd4636f0ddf6b508fb48832f1f
liangkai1 <kingekinge@163.com>
2016-09-07 11:55:12 +0800

    添加连接池

---
+++ app/controllers/cluster/MetaDataManager.scala
@@ -0,0 +1,39 @@
+package controllers
+
+import models.{MetaData, MetaDataList}
+import play.api.libs.json.{JsValue, Json}
+import play.api.mvc.{Action, Controller}
+
+
+/**
+  * Created by king on 16/9/5.
+  */
+object MetaDataManager extends Controller{
+
+
+  def metadata=Action{
+    Ok(views.html.metadata())
+  }
+
+  def metadatas=Action{
+    implicit val residentWrites = Json.writes[MetaData]
+    implicit val clusterListWrites = Json.writes[MetaDataList]
+    val data: JsValue = Json.toJson(MetaDataList(MetaData.findMetaDatas))
+    Ok(data)
+  }
+
+
+
+  def update(id:Int,name:String,unit: String,version:String,url:String)=Action{
+    MetaData.addOrUpdate(MetaData(id,name,unit,version,url))
+    Ok("操作成功")
+  }
+
+
+  def rm(id:String)=Action{
+    MetaData.deleteMetaData(id)
+    Ok("操作成功")
+  }
+
+
+}
